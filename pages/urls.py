4e5bdead8b2e02d07db772cb2d0f7ba1a08e807c
ManuelArenas1 <maarenasl@eafit.edu.co>
2026-02-28 20:27:47 -0500

    <feat(cart)> Implemented cart view, included add to cart and remove from cart fetures, created cart folder with index.html

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import homePageView, aboutPageView, contactPageView, productIndexView, productShowView, productCreateView
+from .views import homePageView, aboutPageView, contactPageView, productIndexView, productShowView, productCreateView, cartRemoveAllView, cartView
 
 urlpatterns = [
     path('', homePageView.as_view(), name ='home'),
@@ -9,5 +9,8 @@ urlpatterns = [
     path('products/<str:id>/', productShowView.as_view(), name='show'),
     path('products/create', productCreateView.as_view(), name='product-create'),
     path('products/<str:id>', productShowView.as_view(), name='show'),
+    path('cart/', cartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', cartView.as_view(), name='cart_add'),
+    path('cart/removeAll', cartRemoveAllView.as_view(), name='cart_removeAll'),
     
 ]
--- pages/views.py
+++ pages/views.py
@@ -122,4 +122,46 @@ class productListView(ListView):
 
         return context
 
+
+class cartView(View):
+    template_name = 'pages/cart/index.html' 
+
+    def get(self, request):
+        products = {}
+        products[121] = {'name': 'TV Samsung', 'Price': '1000'}
+        products[11] = {'name': 'iphone', 'Price': '2000'}
+
+
+        cart_products = {}
+        cart_product_data = request.session.get('cart_product_data', {})
+
+
+        for key, product in products.items():
+            if str(key) in cart_product_data.keys():
+                cart_products[key] = product
+        
+        view_data = {
+            'title': 'Cart - Online Store',
+            'subtitle': 'Shopping Cart',
+            'products': products,
+            'cart_products': cart_products
+        }
+
+        return render(request, self.template_name, view_data)
+
+
+    def post(self, request, product_id):
+        cart_product_data = request.session.get('cart_product_data',{})
+        cart_product_data[product_id] = product_id
+        request.session['cart_product_data'] = cart_product_data
+
+        return redirect('cart_index')
+
+
+class cartRemoveAllView(View):
+    def post(self, request):
+        if 'cart_product_data' in request.session:
+            del request.session['cart_product_data']
+        return redirect('cart_index')
+
         
